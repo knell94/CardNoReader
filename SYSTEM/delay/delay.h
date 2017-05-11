07968420a3df82b5d2184fa5aada5f39b0d58c23
knell94 <zjy333666@163.com>
2017-05-11 21:01:44 +0800

    Update delay.h

--- SYSTEM/delay/delay.h
+++ SYSTEM/delay/delay.h
@@ -1,6 +1,7 @@
 #ifndef __DELAY_H
 #define __DELAY_H 			   
-#include <stm32f10x_lib.h>
+#include <stm32f10x_map.h>
+#include <stm32f10x_nvic.h>
 
 void delay_init(u8 SYSCLK);
 void delay_ms(u16 nms);
